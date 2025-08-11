9a0b870af5d92c90b07f48738cd85d0321982371
Nathan Rawle <nathan.rawle@gmail.com>
2025-08-11 21:36:58 +0100

    add gopath bin to path and styling

--- portables/.precompinit.zsh
+++ portables/.precompinit.zsh
@@ -1,7 +1,10 @@
 # set up completions and path for installed commands
 
-[[ -e '/Users/nathan/google-cloud-sdk/path.zsh.inc' ]] && . '/Users/nathan/google-cloud-sdk/path.zsh.inc'
-[[ -e '/Users/nathan/google-cloud-sdk/completion.zsh.inc' ]] && . '/Users/nathan/google-cloud-sdk/completion.zsh.inc'
+[[ -e '/Users/nathan/google-cloud-sdk/path.zsh.inc' ]] \
+&& . '/Users/nathan/google-cloud-sdk/path.zsh.inc'
+
+[[ -e '/Users/nathan/google-cloud-sdk/completion.zsh.inc' ]] \
+&& . '/Users/nathan/google-cloud-sdk/completion.zsh.inc'
 
 # completions not managed by brew or omz
 [[ -d ~/.zfunc ]] && fpath+=~/.zfunc
@@ -10,6 +13,14 @@
 hash | grep -qE ^zoxide= && eval "$(zoxide init zsh)"
 hash | grep -qE ^pyenv= && eval "$(pyenv init -)"
 hash | grep -qE ^direnv= && eval "$(direnv hook zsh)"
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] \
+&& . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] \
+&& . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+# put go binaries in path:
+[[ -d "${GOPATH:-~/go}/bin" ]] \
+&& path+="$GOPATH/bin"
