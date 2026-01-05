dbc88c2a1e6130f7d4a5c093c6a5e2776d1855dc
Nathan Rawle <nathan.rawle@gmail.com>
2026-01-05 00:01:43 +0000

    nvim: basic snacks lazygit integration

---
+++ portables/.config/nvim/lua/lazynvim/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    lazygit = { enabled = true },
+  },
+  keys = {
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit()
+      end,
+      desc = "LazyGit",
+    },
+  },
+}
