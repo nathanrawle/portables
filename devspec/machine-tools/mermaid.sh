e76797cf07e6ea2adac50db2fc8187240717d5aa
Nathan Rawle <nrawle@libertyglobal.com>
2026-02-03 21:43:40 +0000

    gemini "fixes"

--- devspec/machine-tools/mermaid.sh
+++ devspec/machine-tools/mermaid.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 case "$1" in
-    install) command -v mmdc >/dev/null 2&>1 || echo npm:mermaid ;;
+    install) command -v mmdc >/dev/null 2>&1 || echo npm:@mermaid-js/mermaid-cli ;;
 esac
