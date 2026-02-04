0574433ab78b0947a5d254937c08197fbf88778d
Nathan Rawle <nathan.rawle@gmail.com>
2026-02-04 23:39:23 +0000

    fix: phantom "1" file

--- devspec/machine-tools/luarocks.sh
+++ devspec/machine-tools/luarocks.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 case "$1" in
-    install) command -v luarocks >/dev/null 2&>1 || echo syspkgmgr:luarocks ;;
+    install) command -v luarocks >/dev/null 2>&1 || echo syspkgmgr:luarocks ;;
 esac
