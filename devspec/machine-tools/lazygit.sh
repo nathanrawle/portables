50a0291e10c668c69547730e238657ba65997c5b
Nathan Rawle <nathan.rawle@gmail.com>
2026-02-04 23:39:23 +0000

    fix: phantom "1" file

--- devspec/machine-tools/lazygit.sh
+++ devspec/machine-tools/lazygit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 case "$1" in
-    install) command -v lazygit >/dev/null 2&>1 || echo syspkgmgr:lazygit ;; 
+    install) command -v lazygit >/dev/null 2>&1 || echo syspkgmgr:lazygit ;;
 esac
