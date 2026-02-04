f21a0623abb7b06d53526b7b22d5a91877fc03b1
Nathan Rawle <nathan.rawle@gmail.com>
2026-02-04 23:39:23 +0000

    fix: phantom "1" file

--- devspec/machine-tools/neovim.sh
+++ devspec/machine-tools/neovim.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 case "$1" in
-    install) command -v nvim >/dev/null 2&>1 || echo syspkgmgr:neovim node:neovim ;;
+    install) command -v nvim >/dev/null 2>&1 || echo syspkgmgr:neovim node:neovim ;;
 esac
