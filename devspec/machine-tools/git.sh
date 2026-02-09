dbcf3ddacdc3e40253f6a18203bc2a5a8c8b4e90
Nathan Rawle <nathan.rawle@gmail.com>
2026-02-09 00:46:46 +0000

    bashrc; python; xcode-select

--- devspec/machine-tools/git.sh
+++ devspec/machine-tools/git.sh
@@ -3,13 +3,13 @@ case "$1" in
   install)
     command -v git >/dev/null 2>&1 \
     || case "$OS" in
-         Darwin)
-	   echo self-install
-           ;;
-         *) echo syspkgmgr:git ;;
-       esac
+      Darwin)
+        echo self-install
+        ;;
+      *) echo syspkgmgr:git ;;
+    esac
     ;;
-  self-install) xcode-select install ;;
+  self-install) xcode-select --install ;;
   config)
     echo "Configuring global git excludesfile..."
     git config --global --unset-all core.excludesfile || true # Ignore error if not set
---
+++ devspec/machine-tools/python.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+case "$1" in
+  install) echo "syspkgmgr:python" ;;
