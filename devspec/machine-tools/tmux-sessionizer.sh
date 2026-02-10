1a12d7f4f31cec8b5044d04cb4596d1030bcdfe2
Nathan Rawle <nrawle@libertyglobal.com>
2026-02-10 21:49:24 +0000

    tmux sessionizer installer + ricing

--- devspec/machine-tools/tmux-sessionizer.sh
+++ devspec/machine-tools/tmux-sessionizer.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-case "$1" in
-  install) command -v tmux-sessionizer >/dev/null 2>&1 || echo self-install ;;
-  self-install)
-    sess_path="$PORTABLES"/../tmux-sessionizer/tmux-sessionizer
-    [[ -s "$sess_path" ]] \
-    || git clone --depth 1 https://github.com/ThePrimeagen/tmux-sessionizer.git
-    [[ -x "$sess_path" ]] || chmod +x "$sess_path"
-    mkdir -p ~/.local/scripts
-    ln -sf "$sess_path"/tmux-sessionizer ~/.local/scripts/tmux-sessionizer
-    ;;
-  config)
-    [[ :"$PATH": = *.local/scripts/?:* ]] \
-    || echo "make sure ~/.local/scripts gets added to your path"
-    ;;
-esac
+url=https://github.com/ThePrimeagen/tmux-sessionizer.git
+sauce="${PORTABLES%/*}"
+source="$sauce"/tmux-sessionizer/tmux-sessionizer
+target=~/.local/scripts/tmux-sessionizer
 
+case "$1" in
+install) command -v tmux-sessionizer >/dev/null 2>&1 || echo self-install ;;
+self-install)
+  [[ -s "$source" ]] ||
+    git clone --depth 1 "$url" "$sauce"/tmux-sessionizer
+
+  [[ -x "$source" ]] || chmod +x "$source"
+
+  mkdir -p ~/.local/scripts
+  rm -f "$target"
+  ln -shf "$source" "$target"
+  ;;
+config)
+  ln -shf "$source" "$target"
+  [[ "$PATH": = *.local/scripts:* ]] ||
+    echo "make sure ~/.local/scripts gets added to your path"
+  ;;
+esac
