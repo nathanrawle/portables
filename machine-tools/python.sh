5c7cd89736ce4dcc829ab764e845cc19c83712ec
Nathan Rawle <nathan.rawle@gmail.com>
2026-02-12 23:55:19 +0000

    logging shenanigans

--- machine-tools/python.sh
+++ machine-tools/python.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
+LOG_NAME="${LOG_NAME:+$LOG_NAME.}python:$1"
+functions log >/dev/null 2>&1 || . "$PORTABLES"/log
+
 [[ -n "$OS" ]] || OS="$(uname -s)"
 
 case "$1" in
-install)
-  case "$OS" in
-  # on macOS, I only really need brew python to get the essential system deps, like
-  # openssl & readline
-  Darwin) [[ -x "$(brew --prefix)/bin/python3" ]] || echo "syspkgmgr:python" ;;
-  *) echo "syspkgmgr:python" ;;
-  esac
-  echo "uv:python:--default:--preview-features:python-install-default"
-  ;;
-config)
-  mkdir -p ~/monty
-  if [[ ! -d ~/monty/.venv ]]; then
-    uv venv --directory ~/monty --allow-existing --prompt monty
-  fi
-  uv pip install --directory ~/monty -r ~/.config/python/monty
-  ;;
+  install)
+    case "$OS" in
+      # on macOS, I only really need brew python to get the essential system deps, like
+      # openssl & readline
+      Darwin) [[ -x "$(brew --prefix)/bin/python3" ]] || echo "syspkgmgr:python" ;;
+      *) echo "syspkgmgr:python" ;;
+    esac
+    echo "uv:python:--default:--preview-features:python-install-default"
+    ;;
+  config)
+    mkdir -p ~/monty
+    if [[ ! -d ~/monty/.venv ]]; then
+      log "creating centralised python environment (monty) for sketching"
+      uv venv --directory ~/monty --allow-existing --prompt monty
+    fi
+    log "kitting-out monty"
+    uv pip install -U --directory ~/monty -r ~/.config/python/monty
+    ;;
 esac
--- machine-tools/uv.sh
+++ machine-tools/uv.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+LOG_NAME="${LOG_NAME:+$LOG_NAME.}uv:$1"
+functions log >/dev/null 2>&1 || . "$PORTABLES"/log
 
 [[ -n "$OS" ]] || OS="$(uname -s)"
 case "$1" in
@@ -11,6 +14,8 @@ install)
   fi
   ;;
 self-install)
+  log "begin"
   curl -LsSf https://astral.sh/uv/install.sh | sh
+  log "complete"
   ;;
 esac
