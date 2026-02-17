10410fcbad9419a4a7b0d299768f9557f24c7e75
Nathan Rawle <nathan.rawle@gmail.com>
2026-02-17 22:44:40 +0000

    install symlinks, curl, and git early; include gcm

--- machine-tools/git.sh
+++ machine-tools/git.sh
@@ -8,21 +8,21 @@ case "$1" in
     command -v git >/dev/null 2>&1 \
     || case "$OS" in
       Darwin)
-        echo self-install
+        echo xcode
         ;;
       *) echo syspkgmgr:git ;;
     esac
     ;;
-  self-install)
+  xcode)
     log "installing xcode command-line tools"
     xcode-select --install
-    log "complete"
     ;;
   config)
-    log -d "configuring global git excludesfile…"
+    log -d "configuring global git configurations…"
+    git config --global user.name "$USER_NAME" # these can be passed from private portables
+    git config --global user.email "$USER_EMAIL"
     git config --global --unset-all core.excludesfile 2>/dev/null || true # Ignore error if not set
     git config --global --add core.excludesfile "$HOME/.config/git/ignore"
     git config --global --add core.excludesfile "$HOME/.gitignore"
-    log -d "complete"
     ;;
 esac
