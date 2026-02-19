0679c62a1064e1499bdd70c30167f7c929215ee5
Nathan Rawle <nathan.rawle@gmail.com>
2026-02-19 22:57:08 +0000

    fixing things

--- machine-tools/fd.sh
+++ machine-tools/fd.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+[[ -n "$OS" ]] || OS="$(uname -s)"
+
 case "$1" in
-  install) command -v fd >/dev/null 2>&1 || echo "syspkgmgr:fd" ;;
+  install)
+    case "$OS" in
+      Linux)
+        [[ -n "$ID" ]] || . /etc/os-release
+        case "$ID" in
+          ubuntu|fedora) command -v fdfind >/dev/null 2>&1 || echo "syspkgmgr:fd-find" ;;
+          *) command -v fd >/dev/null 2>&1 || echo "syspkgmgr:fd" ;;
+        esac
+        ;;
+      *) command -v fd >/dev/null 2>&1 || echo "syspkgmgr:fd" ;;
+    esac
 esac
