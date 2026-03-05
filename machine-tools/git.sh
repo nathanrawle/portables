f59e7cd8f07c23bd81000086847bb2fbb31a175a
Nathan Rawle <nathan.rawle@gmail.com>
2026-03-05 21:45:52 +0000

    better git config and zsh logging

--- machine-tools/git.sh
+++ machine-tools/git.sh
@@ -18,13 +18,17 @@ case "$1" in
     xcode-select --install
     ;;
   config)
-    log
-    [[ -n "$GIT_NAME" ]] || read -p 'What name should appear on your commits? ' GIT_NAME
-    [[ -n "$GIT_EMAIL" ]] || read -p 'What email should git use? ' GIT_EMAIL
-    git config --global user.name "$GIT_NAME"
-    git config --global user.email "$GIT_EMAIL"
+    log "configuring…"
+    if [[ -z "$(git config --global user.name)" ]]; then
+      [[ -n "$GIT_NAME" ]] || read -p 'What name should appear on your commits? ' GIT_NAME
+      git config --global user.name "$GIT_NAME"
+    fi
+    if [[ -z "$(git config --global user.email)" ]]; then
+      [[ -n "$GIT_EMAIL" ]] || read -p 'What email should git use? ' GIT_EMAIL
+      git config --global user.email "$GIT_EMAIL"
+    fi
     git config --global --unset-all core.excludesfile 2>/dev/null || true # Ignore error if not set
-    git config --global --add core.excludesfile "$HOME/.config/git/ignore"
     git config --global --add core.excludesfile "$HOME/.gitignore"
+    git config --global --add core.excludesfile "$HOME/.config/git/ignore"
     ;;
 esac
