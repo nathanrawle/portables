630ee00149eef2c94f7d9d311b2a81bbef22da45
Nathan Rawle <nathan.rawle@gmail.com>
2026-03-05 21:23:01 +0000

    little tweaks

--- home/.config/nvim/lua/netrwopts.lua
+++ home/.config/nvim/lua/netrwopts.lua
@@ -6,7 +6,7 @@ end
 
 vim.g.loaded_netrw = nil
 vim.g.loaded_netrwPlugin = nil
-vim.g.netrw_altfile = 1 -- C-^ iuugnores netrw
+vim.g.netrw_altfile = 1 -- C-^ ignores netrw
 vim.g.netrw_winsize = 32 -- explorer width/height when it does split
 vim.g.netrw_list_hide = table.concat(
   vim.list_extend({
