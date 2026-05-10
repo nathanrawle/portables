4c7220219be8a93e4b60161aea4386619e912632
Nathan Rawle <nathan.rawle@gmail.com>
2026-05-10 11:53:05 +0100

    Add csvview.nvim config

---
+++ home/.config/nvim/lua/lazynvim/plugins/csvview.lua
@@ -0,0 +1,64 @@
+return {
+  "hat0uma/csvview.nvim",
+  ft = { "csv", "tsv" },
+  cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle", "CsvViewInfo" },
+  ---@module "csvview"
+  ---@type CsvView.Options
+  opts = {
+    parser = {
+      comments = { "#", "//" },
+    },
+    view = {
+      display_mode = "border",
+      header_lnum = true,
+      sticky_header = {
+        enabled = true,
+      },
+    },
+    keymaps = {
+      -- Text objects for selecting fields.
+      textobject_field_inner = { "if", mode = { "o", "x" } },
+      textobject_field_outer = { "af", mode = { "o", "x" } },
+
+      -- Excel-like navigation.
+      jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+      jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+      jump_next_row = { "<Enter>", mode = { "n", "v" } },
+      jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+    },
+  },
+  config = function(_, opts)
+    require("csvview").setup(opts)
+
+    local function enable(buf)
+      if vim.b[buf].csvview_auto_enable_pending then
+        return
+      end
+
+      vim.b[buf].csvview_auto_enable_pending = true
+      vim.schedule(function()
+        if not vim.api.nvim_buf_is_valid(buf) then
+          return
+        end
+
+        vim.b[buf].csvview_auto_enable_pending = nil
+        local csvview = require("csvview")
+        if not csvview.is_enabled(buf) then
+          csvview.enable(buf)
+        end
+      end)
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("user_csvview", { clear = true }),
+      pattern = { "csv", "tsv" },
+      callback = function(args)
+        enable(args.buf)
+      end,
+    })
+
+    if vim.bo.filetype == "csv" or vim.bo.filetype == "tsv" then
+      enable(0)
+    end
+  end,
+}
