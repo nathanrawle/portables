3c0dcd4958f1ffad7d58156540f0dfe29c2dc866
Nathan Rawle <nathan.rawle@gmail.com>
2026-05-21 23:22:38 +0100

    Modernize Neovim LSP setup

--- home/.config/nvim/lua/lazynvim/plugins/editor-basics.lua
+++ home/.config/nvim/lua/lazynvim/plugins/editor-basics.lua
@@ -12,4 +12,24 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { signs = false },
   },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        sql = { "sqlfluff" },
+        ["yaml.ghaction"] = { "actionlint" },
+      }
+
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
 }
