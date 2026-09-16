cfb6331261670148e370c436886f16513b9e64c5
Nathan Rawle <nathan.rawle@gmail.com>
2026-09-16 01:15:02 +0100

    nvim: highlight custom tmux options

--- home/.config/nvim/queries/tmux/injections.scm
+++ home/.config/nvim/queries/tmux/injections.scm
@@ -7,3 +7,11 @@
     content: (string) @injection.content)
   (#match? @_option "^status-format(\\[[0-9]+\\])?$")
   (#set! injection.language "tmuxf"))
+
+(set_option_directive
+  (command)
+  (option) @_option
+  (value
+    content: (string) @injection.content)
+  (#match? @_option "^[@]")
+  (#set! injection.language "tmuxf"))
